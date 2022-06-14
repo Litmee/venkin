acb20118e6195f9b0fa9488840793f3154633694
litme <wangxinyu.go@qq.com>
2022-06-14 13:15:35 +0800

    引擎启动检查函数添加

--- core/run.go
+++ core/run.go
@@ -8,9 +8,10 @@ import (
 
 // Run 引擎启动
 func Run(c conf.WebConf) {
-	logger.LogRun()
+	logger.LogRun("HTTP 引擎启动中")
 	// 配置全局拦截器
 	conf.IsGlobalInterceptor = c.IsGlobalInterceptor
+	logger.LogRun("拦截器初始化完毕")
 
 	// 跨域参数的预处理
 	if c.AllowOrigin != "" {
@@ -25,9 +26,12 @@ func Run(c conf.WebConf) {
 		allowHeaders = c.AllowHeaders
 		allowHeadersBool = true
 	}
+	logger.LogRun("跨域参数初始化完毕")
+	// 启动检查函数
+	go startCheck(c.Ip + ":" + c.Port)
 	// 启动服务监听
 	err := http.ListenAndServe(c.Ip+":"+c.Port, &GlobalHandler{})
 	if err != nil {
-		panic("http 服务监听启动失败")
+		panic("HTTP 服务监听启动失败")
 	}
 }
